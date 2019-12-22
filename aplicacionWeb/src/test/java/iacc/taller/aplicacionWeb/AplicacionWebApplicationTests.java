c3b6d084d62208255751d254603ae1f549f554b3
Alan.Lizardi <alan.lizardi.albizu@accenture.com>
2019-12-22 17:10:21 -0300

    Commit Eclipse

---
+++ aplicacionWeb/src/test/java/iacc/taller/aplicacionWeb/AplicacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package iacc.taller.aplicacionWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
